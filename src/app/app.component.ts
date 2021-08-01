d3dbe9b4962d802a4be6abc424d06190dace261c
Dennis <dennis.kimani@student.moringaschool.com>
2021-08-01 19:51:49 +0300

    revised goals app

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Goal} from './goal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+// export class AppComponent {
+//   goals:string[];
+//   constructor(){
+//     this.goals = ['Watch Finding Nemo','Buy Cookies','Get new phone case'];
+//   }
+  
+// }
+export class AppComponent {
+
+}
---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,18 @@
+import { Directive,ElementRef,HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+  @HostListener ("click") onClicks(){
+    this.textDeco("underline")
+  }
+  @HostListener ("dblclick") onDoubleClick(){
+    this.textDeco("None")
+  }
+
+  constructor(private elem:ElementRef) { }
+  private textDeco(action:string) {
+    this.elem.nativeElement.style.textDecoration = action;
+  }
+}
